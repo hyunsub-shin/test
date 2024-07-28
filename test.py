9fa24ad9bd8afd332754295f55d75ea54ba57280
subbylife <subbylife@gmail.com>
2024-07-29 08:42:06 +0900

    code chagne

--- test.py
+++ test.py
@@ -9,13 +9,3 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)	#3
 s.connect(('8.8.8.8', 0))
 ip = s.getsockname()[0]
 print(ip)
-
-
-clearScreen(15)
-time.sleep(1)
-origImage = Image.open('text_generate_img.jpg')
-origImage = origImage.resize((1448, 1072))
-drawImage(0, 0, origImage)
-
-time.sleep(1)
-s.close()
